aeb0de6286c1f7e5a564a552c024e17a47a270cb
Bhavya Khatri <133779496+bhavya-shikamaru@users.noreply.github.com>
2025-09-13 12:00:21 +0530

    Create electron.js

---
+++ electron.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // Load Vite dev server in dev mode, or dist index.html in production
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:5173/');
+  } else {
+    win.loadFile(path.join(__dirname, 'dist/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
